a63eb0059615deec771b4b698b1b944e6ed3d860
pieh <misiek.piechowiak@gmail.com>
2024-08-08 11:38:23 +0200

    no avif

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     },
   },
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
     remotePatterns: [
       {
         protocol: "https",
